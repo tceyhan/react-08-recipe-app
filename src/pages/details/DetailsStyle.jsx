2f4fc24a5678b1f66f0513fbe212d9e9a797269c
tceyhan <55tc155@gmail.com>
2022-04-07 21:42:23 +0300

    AppRouter added

--- src/pages/details/DetailsStyle.jsx
+++ src/pages/details/DetailsStyle.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
 
+const DetailsStyle = () => {
+  return (
+    <div>DetailsStyle</div>
+  )
+}
+
+export default DetailsStyle
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,31 @@
 
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
+import Home from "../pages/home/Home";
+import About from "../pages/about/About";
+import Details from "../pages/details/Details";
+import Login from "../pages/login/Login";
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <div>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/details" element={<Details/>}/>
+          
+        </Routes>
+
+
+      <Footer />
+      </BrowserRouter>
+      
+    </div>
   )
 }
 
