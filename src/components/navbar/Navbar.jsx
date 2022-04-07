973fb915bb96aa5ca86ad7ffdd7e9cfaac44a3dd
tceyhan <55tc155@gmail.com>
2022-04-07 22:41:41 +0300

    hamburger menu added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,34 @@
 
 import React from 'react'
+import { Nav, Logo, Hamburger, Menu, MenuLink } from './NavbarStyles';
+import { useState } from 'react';
+// import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>Navbar</div>
+    <div>
+      <Nav>
+        <Logo to ="/home" >
+        <i>{"<Clarusway>"}</i>
+        <span>recipe</span>
+        </Logo>
+        <Hamburger onClick = {() => setIsOpen(!isOpen)}>
+          <span/>
+          <span/>
+          <span/>
+          {/* <GiHamburgerMenu /> */}
+        </Hamburger>
+        <Menu osman = {isOpen}>
+         <MenuLink to="/about" onClick = {() => setIsOpen(!isOpen)}>About</MenuLink>
+         <MenuLink to="/github">Github</MenuLink>
+         <MenuLink to="/" onClick = {() => setIsOpen(!isOpen)}>Logout</MenuLink>
+
+        </Menu>
+
+        
+      </Nav>
+    </div>
   )
 }
 
