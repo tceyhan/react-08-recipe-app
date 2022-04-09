6f20580e4bfbdfbc49415aca5a81bfe92efdf0b7
tceyhan <55tc155@gmail.com>
2022-04-09 11:42:40 +0300

    home component added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,40 +1,62 @@
 
 import axios from 'axios';
-import React, {useState} from 'react';
-import Header from '../../components/header/Header';
+import React, {useState} from 'react'
+import Header from "../../components/header/Header";
+import { ImgDiv, MainContainer,HomeImg } from './HomeStyles';
+import homeSvg from '../../assets/home.svg'
+import RecipeCardComp from "./RecipeCardComp"
 
-const APP_ID = "7164b2bc";
-const APP_KEY = "c9a206eee659e5c15eb6b58c4e86d33a";
+
+const APP_ID = "bfbb3efc"; 
+const APP_KEY = "43faeee790f26cd82b28050d3031619d";
+/* buraya kendi id ve key imizi yaziyoruz*/
+
 
 const Home = () => {
-  const [query, setQuery] = useState("");
-  const [food, setFood] = useState();
-  const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
-  const [meal, setMeal] = useState(mealTypes[0].toLowerCase());
-
-
-
+const [query, setQuery]=useState("");
+const[food, setFood]=useState();
+const mealTypes=["Breakfast", "Lunch","Dinner","Snack","Teatime"]
+const [meal,setMeal]=useState(mealTypes[0].toLowerCase());
   
-  const url = ` https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
-
+const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
+  
   const getData = async () => {
-    if (query) {
+    if(query){ 
       const result = await axios.get(url);
-      setFood(result.data.hits);      
-    }
-    else {
-      console.log("please enter a food");
-    }
-  };
+    setFood(result.data.hits)
+  //  console.log(result);
+    console.log(result.data.hits);
+  
+  }else {
+    console.log("please fill the form");
+  }
+      
 
+
+  }
+  
   return (
     <div>
-      <Header
-      setQuery ={setQuery}
-      getData ={getData}
-      mealTypes={mealTypes}
-      setMeal={setMeal}
-      />
+<Header  setQuery={setQuery}
+getData={getData}
+mealTypes={mealTypes}
+setMeal={setMeal}
+
+/>
+
+{food? (<MainContainer>
+{food.map((liste,index)=>(
+  <RecipeCardComp key={index} recipe1={liste.recipe}/>
+))}
+
+</MainContainer>
+
+):<ImgDiv>
+<HomeImg src={homeSvg}/>
+</ImgDiv>}
+
+
+
 
     </div>
   )
