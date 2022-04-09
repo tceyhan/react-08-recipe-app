4f9312e6d7c1e6418464e26c524211fa784f324b
tceyhan <55tc155@gmail.com>
2022-04-09 12:01:36 +0300

    RecipeCardComp added

--- src/pages/home/RecipeCardComp.jsx
+++ src/pages/home/RecipeCardComp.jsx
@@ -1,9 +1,25 @@
 
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button, RecipeCard, RecipeHeader, RecipeImage } from './HomeStyles'
 
-const RecipeCardComp = () => {
+const RecipeCardComp = ({recipe1}) => {
+  const navigate = useNavigate();
+  const moreClick = () => {
+    navigate("/details", { state: {recipe1}})
+  }
+  
   return (
-    <div>RecipeCardComp</div>
+    <div>
+      <RecipeCard>
+        <RecipeHeader>
+          {recipe1.label}
+        </RecipeHeader>
+        <RecipeImage src={recipe1.image}/>
+
+        <Button onClick={moreClick}>View More</Button>
+      </RecipeCard>
+    </div>
   )
 }
 
