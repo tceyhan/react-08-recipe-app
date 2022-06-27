f325163ec6ce04c5c17ed74587f3a8d0a244ce43
Tarık CEYHAN <84515427+tceyhan@users.noreply.github.com>
2022-06-27 13:41:26 +0300

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,8 +7,8 @@ import homeSvg from '../../assets/home.svg'
 import RecipeCardComp from "./RecipeCardComp"
 
 
-const APP_ID = "bfbb3efc"; 
-const APP_KEY = "43faeee790f26cd82b28050d3031619d";
+const APP_ID = "7164b2bc"; 
+const APP_KEY = "6752e657e8aba29be6a2f75c9a0fc48a";
 /* buraya kendi id ve key imizi yaziyoruz*/
 
 
